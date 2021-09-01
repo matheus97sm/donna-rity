ffa3404c2063a5c6588e59e5e856be1f67dab398
matheus97sm <matheus97sm@gmail.com>
2021-09-01 17:52:13 -0300

    Remoção dos Tags forçados no código

--- header.php
+++ header.php
@@ -21,14 +21,6 @@
 <!--<![endif]-->
 
 <head>
-  <!-- Google Tag Manager -->
-  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-  'https://www.googletagmanager.com/gtm.js?id=%27+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-K5TSH2T');</script>
-  <!-- End Google Tag Manager -->
-
   <meta charset="<?php bloginfo( 'charset' ); ?>" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
 
@@ -48,11 +40,6 @@
 </head>
 
 <body <?php body_class(); ?>>
-  <!-- Google Tag Manager (noscript) -->
-  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K5TSH2T"
-  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-  <!-- End Google Tag Manager (noscript) -->
-  
   <header class="header" data-animar>
     <div class="container">
       <a href="<?=site_url()?>" class="header-logo">
